b445f48b38eaa79ceee11bcf0012f833f23b46b9
Julian Henry <julianplushenry@gmail.com>
2023-10-05 13:58:01 -0500

    Turns out the validator is invalid

--- pyeqn_validator.py
+++ pyeqn_validator.py
@@ -6,40 +6,37 @@ import re, os
 class PyEqnValidator:
     def __init__(self, infile: str):
         self.infile = infile
+        self.eqn_regex = "\d{1,2}-\d{1,2}(?:\w)?"
 
     def reveal_blank_eqn_names(self):
         ix = 1
-        for o in os.listdir(os.getcwd() + self.infile):
-            if o[0] == "_":
-                continue
-            with open(os.getcwd() + self.infile) as file:
-                for l in file.readlines():
-                    if x := re.compile("\d{1,2}-\d{1,2}\w").findall(l):
-                        eqn_number = x[0]
-                        if len(l) < 10:
-                            ix += 1
-                            # print(ix, l.strip(), "needs name!")
-
-    def see_which_notes_are_valid_Python(self):
-        for o in os.listdir(os.getcwd() + "/chapters"):
-            if o[0] == "_":
-                continue
-            with open(os.getcwd() + "/chapters/" + o) as file:
-                eqn_number = ""
-                for l in file.readlines():
-                    if x := re.compile("\d{1,2}-\d{1,2}").findall(l):
-                        eqn_number = x[0]
-                    if "=" in l and ":" not in l.split("=")[0]:
-                        self.parse_eqn(l.strip().split("#")[0])
+        with open(os.getcwd() +'/' + self.infile) as file:
+            for l in file.readlines():
+                # Use regular expression to find patterns like "1-2a"
+                # print(l)
+                if eqn_number := re.compile(self.eqn_regex).findall(l):
+                    print(eqn_number)
+                # Use regular expression to find patterns like "1-2"
+                if x := re.compile(self.eqn_regex).findall(l):
+                    eqn_number = x[0]
+                # Check if the line contains "=" but not followed by ":"
+                if "=" in l and ":" not in l.split("=")[0]:
+                    self.parse_eqn(l.strip().split("#")[0])
 
     def parse_eqn(self, l: str):
         try:
+            # Try to evaluate the expression in the line
             eval(l)
         except SyntaxError as se:
+            # If there's a syntax error, extract the tokens and print the line
             to_tokens = se.text.split("=")[0]
             y = re.compile("\w[A-Za-z0-9\-\_]+").findall(to_tokens)
-            for o in y:
-                print(o, "=", 1.1)
             print(l)
         except NameError as ne:
+            # If there's a name error, ignore it
             pass
+
+if __name__ == '__main__':
+    infile = 'pyeqn_examples/simple.pyeqn'
+    pev = PyEqnValidator(infile)
+    pev.reveal_blank_eqn_names()
